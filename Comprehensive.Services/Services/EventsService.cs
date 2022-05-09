d03cbe5ac4eb72a1c5f0446e61707858230e7008
V <vitoralcanfor@hotmail.com>
2022-05-09 16:18:05 -0300

    EventsService inheritance

--- Comprehensive.Services/Services/EventsService.cs
+++ Comprehensive.Services/Services/EventsService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Comprehensive.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Comprehensive.Services.Services
 {
-    internal class EventsService 
+    internal class EventsService : IEventsService
     {
     }
 }
