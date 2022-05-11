1d8fce1e989df11954e18a799f9eed7d77939fe6
V <vitoralcanfor@hotmail.com>
2022-05-11 16:05:31 -0300

    Created View model, Views

---
+++ Comprehensive.Web/ViewModel/Events/EventsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Comprehensive.Web.ViewModel.Events
+{
+    public class EventsViewModel
+    {
+        [Display(Name = "EventId")]
+        public long EventId { get; set; }
+
+        [Display(Name = "EventName")]
+        public string? EventName { get; set; }
+
+        [Display(Name = "EventDate")]
+        public DateTime Date { get; set; }
+
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Display(Name = "State")]
+        public string State { get; set; }
+
+        [Display(Name = "EventAddress")]
+        public string? Address { get; set; }
+
+        [Display(Name = "IsValid")]
+        public bool? IsValid { get; set; }
+    }
+}
